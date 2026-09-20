dae741a59416ce785df24f2d65dcacba1ef2ee1d
RadiatorTwo <radiatortwo@gmail.com>
2026-09-21 00:06:42 +0200

    Add plugin scaffolding

---
+++ SystemdPlugin.cs
@@ -0,0 +1,31 @@
+using LoupixDeck.PluginSdk;
+
+namespace LoupixDeck.Plugin.Systemd;
+
+public sealed class SystemdPlugin : LoupixPlugin
+{
+    private IPluginHost? _host;
+
+    public override PluginMetadata Metadata { get; } = new()
+    {
+        Id = "systemd",
+        Name = "Systemd",
+        Version = new Version(1, 0, 0),
+        SdkVersion = new Version(1, 24, 0),
+        Author = "",
+        Description = ""
+    };
+
+    public override void Initialize(IPluginHost host)
+    {
+        _host = host;
+    }
+
+    public override IEnumerable<IPluginCommand> GetCommands() => [];
+
+    /// <summary>
+    /// Translates English text the plugin builds while running. Descriptor text is
+    /// translated by the host through the same strings.&lt;code&gt;.json files.
+    /// </summary>
+    private string Tr(string english) => _host?.Tr(english) ?? english;
+}
